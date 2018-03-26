3ad0b50e883ffbf04cc1615512d17ab74d9776ce
Roland Huß <roland@ro14nd.de>
2018-03-26 15:29:06 +0200

    chore: Minor doc fixes + fix of an error message (#249)
    * chore: Minor spacing fix for error message
    
    * fix: Minor typo fix in getting_started guide
    
    * fix: Correct link to example config
    
    * chore: Fix some formatting in getting_started.md
    
    * chore: Add some extra info how to provision the updated apb
    
    * chore(getting_started): wording
    
    * fix(getting_started): typo

--- src/apb/engine.py
+++ src/apb/engine.py
@@ -957,7 +957,7 @@ def push_apb(registry, tag):
         openshift_config.load_kube_config()
         api_key = openshift_client.Configuration().get_api_key_with_prefix('authorization')
         if api_key is None:
-            raise Exception("No api key found in kubeconfig. NOTE: system:admin" +
+            raise Exception("No api key found in kubeconfig. NOTE: system:admin " +
                             "*cannot* be used with apb, since it does not have a token.")
         token = api_key.split(" ")[1]
         username = "developer" if is_minishift() else "unused"
